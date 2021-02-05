b9e4a9ad983f3839386c27b475105e30dd7c83c0
Vresod <lukebinkofsky@gmail.com>
2021-02-05 11:26:14 -0500

    switch to discord.py extensions and use webhooks to imitate better

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
 import discord
+from discord.ext import commands
 from extras import *
 
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
 
-client = discord.Client()
+client = commands.Bot(command_prefix="s?")
 
 @client.event
 async def on_ready():
 	print("logged in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-	if message.author == client.user or message.author.bot:
-		return
-	if not message.content.startswith("?spoiler"):
-		return
-	text = message.content[9:]
-	images = await attachments_to_files(message.attachments,True)
-
-	await message.delete()
-	await message.channel.send(content="**{0}**\n{1}".format(message.author.name,text),files=images)
+@client.command(aliases=["sp"])
+async def spoiler(ctx,*text):
+	images = await attachments_to_files(ctx.message.attachments,True)
+	txt = " ".join(text)
+	await ctx.message.delete()
+	imitated = ctx.author
+	avatar = await imitated.avatar_url_as(format="png").read()
+	hook = await ctx.channel.create_webhook(name=imitated.display_name,avatar=avatar)
+	await hook.send(content=txt,files=images)
 
 client.run(token)
